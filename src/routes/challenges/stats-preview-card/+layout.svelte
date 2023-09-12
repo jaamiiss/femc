ccf5c207a426b1fb1e59332021562d30b528d40d
James Magallanes <fridaysyckness@gmail.com>
2023-09-12 17:04:08 +0800

    upt pkg
    +c

---
+++ src/routes/challenges/stats-preview-card/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+    import '../../app.css'
+</script>
+<svelte:head>
+    <title>Stats Review Card</title>
+</svelte:head>
+<slot />
---
+++ src/routes/challenges/stats-preview-card/+page.svelte
@@ -0,0 +1,4 @@
+<script>
+    import StatsPreviewCard from "$lib/components/StatsPreviewCard.svelte";
+</script>
+<StatsPreviewCard/>
